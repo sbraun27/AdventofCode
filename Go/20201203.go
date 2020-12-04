d2dce385526750154981d89a59debfb929693b4d
sbraun27 <sbraun27@gmail.com>
2020-12-03 18:10:01 -0700

    Adding Go Solution for Day 2

---
+++ Go/20201203.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+	"time"
+)
+
+func readFile(fileName string) []string {
+	f, _ := os.Open(fileName)
+
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+
+	lines := []string{}
+
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	return lines
+}
+
+func calcTrees(slopeMap []string, steps []int) int {
+	column := 0
+	targetCol := 0
+	rowLength := len(slopeMap[0])
+
+	treeNum := 0
+
+	for row := 0; row < len(slopeMap); row += steps[0] {
+		if column >= rowLength {
+			targetCol = column % rowLength
+		} else {
+			targetCol = column
+		}
+
+		currentRow := strings.Split(slopeMap[row], "")
+		if currentRow[targetCol] == "#" {
+			treeNum++
+		}
+
+		column += steps[1]
+	}
+	return treeNum
+}
+
+func main() {
+	slopeMap := readFile("../input/day3.txt")
+	fmt.Println(slopeMap[0])
+	steps := []int{1, 3}
+
+	start := time.Now()
+	part1Result := calcTrees(slopeMap, steps)
+	fmt.Println("Part 1 result: ", part1Result, "Took: ", time.Since(start))
+
+	part2Results := 1
+
+	part2Steps := [][]int{[]int{1, 1}, []int{1, 3}, []int{1, 5}, []int{1, 7}, []int{2, 1}}
+
+	start = time.Now()
+	for _, steps := range part2Steps {
+		part2Results = part2Results * calcTrees(slopeMap, steps)
+	}
+	fmt.Println("Part 2 result: ", part2Results, "Took: ", time.Since(start))
+}
